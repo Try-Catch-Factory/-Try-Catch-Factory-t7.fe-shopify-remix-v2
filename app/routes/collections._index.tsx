c0eeaac15062c5b7c1321f63249a1f2587e8fce1
Jetmarte <obcollazos@gmail.com>
2024-07-19 03:14:46 -0400

    basic style of collections

--- app/routes/collections._index.tsx
+++ app/routes/collections._index.tsx
@@ -1,46 +1,51 @@
-import {useLoaderData, Link} from '@remix-run/react';
-import {json, type LoaderFunctionArgs} from '@netlify/remix-runtime';
-import {Pagination, getPaginationVariables, Image} from '@shopify/hydrogen';
-import type {CollectionFragment} from 'storefrontapi.generated';
+import { useLoaderData, Link } from '@remix-run/react';
+import { json, type LoaderFunctionArgs } from '@netlify/remix-runtime';
+import { Pagination, getPaginationVariables, Image } from '@shopify/hydrogen';
+import type { CollectionFragment } from 'storefrontapi.generated';
 
-export async function loader({context, request}: LoaderFunctionArgs) {
+export async function loader({ context, request }: LoaderFunctionArgs) {
   const paginationVariables = getPaginationVariables(request, {
     pageBy: 4,
   });
 
-  const {collections} = await context.storefront.query(COLLECTIONS_QUERY, {
+  const { collections } = await context.storefront.query(COLLECTIONS_QUERY, {
     variables: paginationVariables,
   });
 
-  return json({collections});
+  return json({ collections });
 }
 
 export default function Collections() {
-  const {collections} = useLoaderData<typeof loader>();
+  const { collections } = useLoaderData<typeof loader>();
 
   return (
-    <div className="collections">
-      <h1>Collections</h1>
-      <Pagination connection={collections}>
-        {({nodes, isLoading, PreviousLink, NextLink}) => (
-          <div>
-            <PreviousLink>
-              {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
-            </PreviousLink>
-            <CollectionsGrid collections={nodes} />
-            <NextLink>
-              {isLoading ? 'Loading...' : <span>Load more ↓</span>}
-            </NextLink>
-          </div>
-        )}
-      </Pagination>
+    <div className="collections ">
+     
+      <div className='flex flex-col justify-center items-center h-screen'>
+        
+        <Pagination connection={collections}>
+          {({ nodes, isLoading, PreviousLink, NextLink }) => (
+            <div className=' w-[1200px] '>
+              <PreviousLink>
+                {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
+              </PreviousLink>
+              <CollectionsGrid collections={nodes} />
+              <NextLink>
+                {isLoading ? 'Loading...' : <span>Load more ↓</span>}
+              </NextLink>
+            </div>
+          )}
+        </Pagination>
+
+      </div>
+
     </div>
   );
 }
 
-function CollectionsGrid({collections}: {collections: CollectionFragment[]}) {
+function CollectionsGrid({ collections }: { collections: CollectionFragment[] }) {
   return (
-    <div className="collections-grid">
+    <div className="collections-grid m-2">
       {collections.map((collection, index) => (
         <CollectionItem
           key={collection.id}
@@ -60,22 +65,25 @@ function CollectionItem({
   index: number;
 }) {
   return (
-    <Link
-      className="collection-item"
-      key={collection.id}
-      to={`/collections/${collection.handle}`}
-      prefetch="intent"
-    >
-      {collection?.image && (
-        <Image
-          alt={collection.image.altText || collection.title}
-          aspectRatio="1/1"
-          data={collection.image}
-          loading={index < 3 ? 'eager' : undefined}
-        />
-      )}
-      <h5>{collection.title}</h5>
-    </Link>
+    <div className='w-full '>
+      <Link
+        className="collection-item "
+        key={collection.id}
+        to={`/collections/${collection.handle}`}
+        prefetch="intent"
+      >
+        {collection?.image && (
+          <Image 
+            alt={collection.image.altText || collection.title}
+            aspectRatio="1/1"
+            data={collection.image}
+            loading={index < 3 ? 'eager' : undefined}
+          />
+        )}
+        <h5>{collection.title}</h5>
+      </Link>
+    </div>
+
   );
 }
 
